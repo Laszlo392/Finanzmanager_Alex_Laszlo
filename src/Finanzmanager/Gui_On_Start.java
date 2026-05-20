084e17bc524da3945b3bf75810b67bb42b2420fc
Alex Kovacs <4125@htl.rennweg.at>
2026-05-20 17:32:52 +0200

    Gui begonnen

---
+++ src/Finanzmanager/Gui_On_Start.java
@@ -0,0 +1,11 @@
+package Finanzmanager;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class Gui_On_Start extends Application{
+    @Override
+    public void start(Stage stage) throws Exception {
+
+    }
+}
